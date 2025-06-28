3d15e73cd99f34f3ce81fd2618dc590b49d7cdcd
Jesus Junior <43008669+yisas97@users.noreply.github.com>
2025-06-28 09:02:37 -0500

    configuracion docker y despliegue

---
+++ execute-sonarqube.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo Ruta de Java HOME...
+REM Java 21
+set JAVA_HOME=C:\Users\JESUS\.jdks\corretto-17.0.11
+
+echo A%dir_PATH%
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+echo Verificacion del JAVA
+java -version
+
+echo Generar el war...
+REM mvn clean install -DskipTests
+
+echo Genera la evaluación de SONAR
+set TOKEN=sqp_a52ac44969f0c8c0b7b69f1d62a4ffb99cceb6f4
+call mvn clean verify -DskipTests sonar:sonar -Dsonar.projectKey=comercio-electronico-seguridad  -Dsonar.projectName='comercio-electronico-seguridad' -Dsonar.host.url=http://localhost:9000 -Dsonar.token=sqp_a52ac44969f0c8c0b7b69f1d62a4ffb99cceb6f4
+
+
+REM echo Levantando con docker
+REM echo Verificacion si esta levantado...
+REM docker compose down
+
+REM echo Builder la imagen:
+REM docker compose build
+
+REM echo Levantar el contenedor:
+REM docker compose up -d
